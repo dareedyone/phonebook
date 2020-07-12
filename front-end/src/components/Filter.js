655c2c8a0acbe4a2358d91eeb7bce2cf52b4c661
Ridwan Ijadunola <dareedyone@gmail.com>
2020-07-12 12:59:56 +0100

    files restructured

--- front-end/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Filter = ({ newFilter, doFiltering }) => (
-  <div>
-    Filter shown with: <input value={newFilter} onChange={doFiltering} />
-  </div>
-);
-
-export default Filter;
