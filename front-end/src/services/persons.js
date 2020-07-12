6c80cb4bccd5fd8db263f216ad1d66ae0eda0b5e
Ridwan Ijadunola <dareedyone@gmail.com>
2020-07-12 11:54:04 +0100

    3.9 phonebook backend step 9 completed

---
+++ front-end/src/services/persons.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const baseUrl = "/api/persons";
+export const getAll = () => axios.get(baseUrl).then((res) => res.data);
+
+export const create = (newPerson) =>
+  axios.post(baseUrl, newPerson).then((res) => res.data);
+export const destroy = (id) => axios.delete(`${baseUrl}/${id}`);
+export const replace = (id, replacer) =>
+  axios.put(`${baseUrl}/${id}`, replacer).then((res) => res.data);
