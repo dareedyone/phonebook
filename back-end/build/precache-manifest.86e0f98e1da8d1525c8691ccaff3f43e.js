5ebd377f30b51ba37bc1bd331207d4022c138abb
Ridwan Ijadunola <dareedyone@gmail.com>
2020-07-14 17:17:47 +0100

    uibuild

---
+++ back-end/build/precache-manifest.86e0f98e1da8d1525c8691ccaff3f43e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aac19398558df932d5ddf9c0f13f8a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbea8d7572b545ad93ae",
+    "url": "/static/css/main.3a8abf91.chunk.css"
+  },
+  {
+    "revision": "f24c868740ab0f7c25f5",
+    "url": "/static/js/2.4b8e90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b8e90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbea8d7572b545ad93ae",
+    "url": "/static/js/main.0376c241.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
